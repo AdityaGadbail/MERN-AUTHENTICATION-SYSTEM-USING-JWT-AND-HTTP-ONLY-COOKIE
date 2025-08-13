4e5f6b864d8cb39ad2379a8cbb34c8c0d5f190cd
Aditya Gadbail <163659268+AdityaGadbail@users.noreply.github.com>
2025-08-13 21:44:00 +0530

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,7 +3,7 @@ import userAuth from "../middleware/userAuth.js";
 import { getUserData } from "../controller/userController.js";
 
 const userRouter = express.Router();
-
+// Endpoint for get users data
 userRouter.get('/data',userAuth,getUserData);
 
 export default userRouter;
