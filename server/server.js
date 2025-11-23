0bcc80be4916a0cfa659b20ede06f65ca19b0a7e
AdityaGadbail <adityagadbail003@gmail.com>
2025-11-23 18:42:25 +0530

    update for nodemailer

--- server/server.js
+++ server/server.js
@@ -15,12 +15,12 @@ connectDB();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:5173',
-  'http://localhost:5173',
+  // 'http://localhost:5173',
   'https://mern-authentication-system-jw.vercel.app',
   'https://mern-authentication-system-using-jwt-and.vercel.app'
 ];
 
-// MIDLEWARES
+// MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
